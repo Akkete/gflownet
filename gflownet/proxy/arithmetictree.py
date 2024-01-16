91f2d9eba8086d313dba904418b6b713d9b138a5
Akkete <akseli.anttonen@aalto.fi>
2024-01-16 21:59:08 +0200

    improve reward function

--- gflownet/proxy/arithmetictree.py
+++ gflownet/proxy/arithmetictree.py
@@ -6,19 +6,27 @@ from torchtyping import TensorType
 from gflownet.proxy.base import Proxy
 
 class ArithmeticScorer(Proxy):
-    def __init__(self, stock: List[int] = [1, -1, 2, -2, 3, -3], **kwargs):
+    def __init__(self, normalize: bool = True, **kwargs):
         super().__init__(**kwargs)
-        self.stock = stock
+        self.normalize = normalize
 
-    def setup(self, env=None):
+    def setup(self, env = None):
         if env:
             self.stock = env.stock
+            self.max_n_nodes = env.max_n_nodes
+
+    @property
+    def norm(self):
+        if self.normalize:
+            return 1.0 * self.max_n_nodes
+        else:
+            return 1.0
 
     def __call__(
         self, states: TensorType["batch", "state_dim"]
     ) -> TensorType["batch"]:
         # dummy test reward 1: more numbers in stock is better
         in_stock = sum(states[:, :, 0]==i for i in self.stock).bool()
-        return in_stock.sum(axis=1)
+        return in_stock.sum(axis=1) / self.norm
         # dummy test reward 2: more bigger numbers is better
-        # return states.sum(axis=(1, 2))
+        # return -1.0 * states.sum(axis=(1, 2))
