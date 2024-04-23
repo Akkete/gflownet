cee061f21ef0de9f89f706070a91aa89c6498798
Akkete <akseli.anttonen@aalto.fi>
2024-04-23 11:34:37 +0300

    networkx representation of state

--- gflownet/proxy/reactiontree.py
+++ gflownet/proxy/reactiontree.py
@@ -5,6 +5,8 @@ from torchtyping import TensorType
 
 from gflownet.proxy.base import Proxy
 
+from gflownet.envs.reactiontree import ReactionTree
+
 class ReactionTreeScorer(Proxy):
     def __init__(
         self, 
@@ -36,21 +38,18 @@ class ReactionTreeScorer(Proxy):
                     self.base_reward += -self.leaf_not_in_stock_reward * (self.max_n_nodes - 1.0)
                 if self.reaction_count_reward < 0.0:
                     self.base_reward += -self.reaction_count_reward * (env.max_reactions)
+            self.num_reaction_classes = len(env.templates)
 
-    def __call__(
-        self, states: TensorType["batch", "state_dim"]
-    ) -> TensorType["batch"]:
-        leaf = states[:, :, -1] == 0
-        in_stock = states[:, :, -2] == 1
-        in_stock_count = in_stock.sum(axis=-1)
-        not_in_stock = states[:, :, -2] == 0
-        leaf_not_in_stock = leaf & not_in_stock
-        leaf_not_in_stock_count = leaf_not_in_stock.sum(axis=-1)
-        leaf_in_stock = leaf & in_stock
-        leaf_in_stock_count = leaf_in_stock.sum(axis=-1)
-        reaction_count = (states[:, :, -3] != -1).sum(axis=-1)
-        incomplete = 1.0 * leaf_not_in_stock.any(axis=-1)
-        is_complete = 1.0 - incomplete
+    def score_state(self, state: ReactionTree) -> float:
+        is_complete = state.is_complete()
+        leaf_in_stock_count = 0
+        leaf_not_in_stock_count = 0
+        for leaf_idx in state.get_leaf_nodes():
+                if state[leaf_idx]["in_stock"]:
+                    leaf_in_stock_count += 1
+                else:
+                    leaf_not_in_stock_count += 1
+        reaction_count = state.n_reactions
         score = sum([
             self.base_reward,
             self.completion_reward * is_complete,
@@ -58,4 +57,13 @@ class ReactionTreeScorer(Proxy):
             self.leaf_not_in_stock_reward * leaf_not_in_stock_count,
             self.reaction_count_reward * reaction_count,
         ])
-        return -1.0 * score
+        return score
+
+    def __call__(
+        self, states: List[ReactionTree]
+    ) -> TensorType["batch"]:
+        scores = torch.tensor(
+            list(map(self.score_state, states)), 
+            device=self.device
+        )
+        return -1.0 * scores
