760021c1cadf13a1ccead10881374f6a42ef3fdd
A.Antt <akseli.anttonen@aalto.fi>
2024-07-10 16:53:23 +0300

    don't ask for input

--- scripts/eval_gflownet.py
+++ scripts/eval_gflownet.py
@@ -230,8 +230,8 @@ def main(args):
             dct = {k: v + tmp_dict[k] for k, v in dct.items()}
         pickle.dump(dct, open(output_dir / f"{prefix}_samples.pkl", "wb"))
 
-        if "y" in input("Delete temporary files? (y/n)"):
-            shutil.rmtree(tmp_dir)
+        # if "y" in input("Delete temporary files? (y/n)"):
+        shutil.rmtree(tmp_dir)
 
 
 if __name__ == "__main__":
